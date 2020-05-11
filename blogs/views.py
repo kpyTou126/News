8fc26ef1d6876044be709fb07f13cfc68607d56e
kpyTou126 <melihov.23032004@mail.ru>
2020-05-12 00:50:19 +0400

    Protection from editing other people's posts.

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import BlogPost
 from .forms import PostForm, EditForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -9,6 +11,7 @@ def index(request):
 	context = {"blog_posts": blog_posts}
 	return render(request, "blogs/index.html", context)
 
+@login_required
 def new_post(request):
 	if request.method !=  "POST":
 		# Просто загрузка формы
@@ -17,22 +20,27 @@ def new_post(request):
 		# Обработка данных.
 		form = PostForm(data=request.POST)
 		if form.is_valid():
+			new_post = form.save(commit=False)
+			new_post.owner = request.user
 			form.save()
 			return redirect("blogs:index")
 	context = {"form": form}
 	return render(request, "blogs/new_post.html", context)
 
+@login_required
 def edit_post(request, post_id):
 	post = BlogPost.objects.get(id=post_id)
-
-	if request.method != "POST":
-		# Исходный запрос. Форма заполняется данными текущей записи.
-		form = EditForm(instance=post)
+	if post.owner == request.user:
+		if request.method != "POST":
+			# Исходный запрос. Форма заполняется данными текущей записи.
+			form = EditForm(instance=post)
+		else:
+			# Отправка данных POST. Обработать данные.
+			form = EditForm(instance=post, data=request.POST)
+			if form.is_valid():
+				form.save()
+				return redirect("blogs:index")
+		context = {"post": post, "form": form}
+		return render(request, "blogs/edit_post.html", context)
 	else:
-		# Отправка данных POST. Обработать данные.
-		form = EditForm(instance=post, data=request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect("blogs:index")
-	context = {"post": post, "form": form}
-	return render(request, "blogs/edit_post.html", context)
+		raise Http404
